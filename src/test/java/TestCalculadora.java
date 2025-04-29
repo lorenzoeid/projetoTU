6711b26ec9948a23cd3f95fbd2ed1f8271108d8a
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-04-29 09:50:42 -0300

    Testes unitários da calculadora

---
+++ src/test/java/TestCalculadora.java
@@ -0,0 +1,71 @@
+import calculadora.Calculadora;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 1072419633
+ */
+public class TestCalculadora {
+    
+    @Test
+    void testGetSoma(){
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getSoma();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    
+    @Test
+    void testGetDiferenca(){
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getDiferenca();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    
+    @Test
+    void testGetProduto(){
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    
+    @Test
+    void testGetQuociente(){
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getQuociente();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    
+    public TestCalculadora() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
